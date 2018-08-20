46e8cad80555a0de62b57075d690dad7c35ccbd5
Anshdeep Singh <anshdeep.1494@gmail.com>
2018-08-20 23:51:01 +0530

    setup firebase and used firebase auth for registration

--- app/src/main/java/com/anshdeep/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/anshdeep/kotlinmessenger/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.anshdeep.kotlinmessenger
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 /**
@@ -16,6 +17,10 @@ class LoginActivity : AppCompatActivity() {
         login_button_login.setOnClickListener {
             val email = email_edittext_login.text.toString()
             val password = password_edittext_login.text.toString()
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener {  }
+                    .addOnFailureListener {  }
         }
 
         back_to_register_textview.setOnClickListener {

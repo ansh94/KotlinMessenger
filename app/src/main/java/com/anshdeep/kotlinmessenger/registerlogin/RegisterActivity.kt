22d5f5f0437dfdbb319072d37f5ca78814c1ba1e
Anshdeep Singh <anshdeep.1494@gmail.com>
2018-09-23 20:15:42 +0530

    image rotation bug : WIP

--- app/src/main/java/com/anshdeep/kotlinmessenger/registerlogin/RegisterActivity.kt
+++ app/src/main/java/com/anshdeep/kotlinmessenger/registerlogin/RegisterActivity.kt
@@ -2,9 +2,12 @@ package com.anshdeep.kotlinmessenger.registerlogin
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Matrix
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.media.ExifInterface
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
@@ -16,7 +19,13 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_register.*
+import java.io.File
 import java.util.*
+
+
+
+
+
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -55,13 +64,62 @@ class RegisterActivity : AppCompatActivity() {
 
             selectedPhotoUri = data.data
 
+
             val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+//            val exif = ExifInterface(selectedPhotoUri!!.path)
+//            val rotation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+//
+//            val rotationInDegrees = exifToDegrees(rotation)
+
+            var isCamera = File(selectedPhotoUri!!.path).length() > 0
+
+            Log.d("RegisterActivity","image path : " + selectedPhotoUri!!.path.toString())
+
+//            Picasso.get().load(selectedPhotoUri).rotate(-90.toFloat()).into(selectphoto_imageview_register)
 
             selectphoto_imageview_register.setImageBitmap(bitmap)
 
             selectphoto_button_register.alpha = 0f
 
         }
+    }
+
+    private fun exifToDegrees(exifOrientation: Int): Int {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270
+        }
+        return 0
+    }
+
+
+
+    fun modifyOrientation(bitmap: Bitmap, image_absolute_path: String): Bitmap {
+        val ei = ExifInterface(image_absolute_path)
+        val orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+        return when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(bitmap, 90.toFloat())
+            ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(bitmap, 180.toFloat())
+            ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(bitmap, 270.toFloat())
+            ExifInterface.ORIENTATION_FLIP_HORIZONTAL -> flipImage(bitmap, true, false)
+            ExifInterface.ORIENTATION_FLIP_VERTICAL -> flipImage(bitmap, false, true)
+            else -> bitmap
+        }
+    }
+
+    private fun rotateImage(bitmap: Bitmap, degrees: Float): Bitmap {
+        val matrix = Matrix()
+        matrix.postRotate(degrees)
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+    }
+
+    private fun flipImage(bitmap: Bitmap, horizontal: Boolean, vertical: Boolean): Bitmap {
+        val matrix = Matrix()
+        matrix.preScale((if (horizontal) -1 else 1).toFloat(), (if (vertical) -1 else 1).toFloat())
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
     }
 
     private fun performRegistration() {
